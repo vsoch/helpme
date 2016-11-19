d2a2d9847ec2231652bc2f2266353ec876c7f2ac
vsoch <vsochat@stanford.edu>
2016-11-19 05:24:47 -0800

    renamed:    PROTOCOL.mdwn -> docs/PROTOCOL.mdwn 	new file:   helpme/__init__.py 	new file:   helpme/helpers/__init__.py 	new file:   setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='helpme',
+      version='1.0',
+      description='command line daemon for user help',
+      author='Vanessa Sochat, Karl Kornell',
+      author_email='vsochat@stanford.edu',
+      url='https://www.github.com/researchapps/helpme',
+      packages=['helpme'],
+      )
