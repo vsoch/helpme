d64d99eba6cd39074fdcd923b6fe5fa30518ed6c
Vanessa Sochat <vsochat@stanford.edu>
2019-12-09 16:44:07 -0700

    updating docstrings
    Signed-off-by: Vanessa Sochat <vsochat@stanford.edu>

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
-'''
-Copyright (C) 2018-2019 Vanessa Sochat.
+"""
+
+Copyright (C) 2018-2020 Vanessa Sochat.
 
 This program is free software: you can redistribute it and/or modify it
 under the terms of the GNU Affero General Public License as published by
@@ -16,7 +17,7 @@ License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-'''
+"""
 
 from setuptools import setup, find_packages
 import codecs
@@ -28,16 +29,18 @@ import os
 
 def get_lookup():
     lookup = dict()
-    version_file = os.path.join('helpme', 'version.py')
+    version_file = os.path.join("helpme", "version.py")
     with open(version_file) as filey:
         exec(filey.read(), lookup)
     return lookup
 
+
 # Read in requirements
 
-def get_reqs(lookup=None, key='INSTALL_REQUIRES'):
-    '''get requirements, meaning reading in requirements and versions from
-    the lookup obtained with get_lookup'''
+
+def get_reqs(lookup=None, key="INSTALL_REQUIRES"):
+    """get requirements, meaning reading in requirements and versions from
+    the lookup obtained with get_lookup"""
 
     if lookup == None:
         lookup = get_lookup()
@@ -47,31 +50,31 @@ def get_reqs(lookup=None, key='INSTALL_REQUIRES'):
         module_name = module[0]
         module_meta = module[1]
         if "exact_version" in module_meta:
-            dependency = "%s==%s" %(module_name,module_meta['exact_version'])
+            dependency = "%s==%s" % (module_name, module_meta["exact_version"])
         elif "min_version" in module_meta:
-            if module_meta['min_version'] == None:
+            if module_meta["min_version"] == None:
                 dependency = module_name
             else:
-                dependency = "%s>=%s" %(module_name,module_meta['min_version'])
+                dependency = "%s>=%s" % (module_name, module_meta["min_version"])
         install_requires.append(dependency)
     return install_requires
 
 
 # Make sure everything is relative to setup.py
-install_path = os.path.dirname(os.path.abspath(__file__)) 
+install_path = os.path.dirname(os.path.abspath(__file__))
 os.chdir(install_path)
 
 # Get version information from the lookup
 lookup = get_lookup()
-VERSION = lookup['__version__']
-NAME = lookup['NAME']
-AUTHOR = lookup['AUTHOR']
-AUTHOR_EMAIL = lookup['AUTHOR_EMAIL']
-PACKAGE_URL = lookup['PACKAGE_URL']
-KEYWORDS = lookup['KEYWORDS']
-DESCRIPTION = lookup['DESCRIPTION']
-LICENSE = lookup['LICENSE']
-with open('README.md') as filey:
+VERSION = lookup["__version__"]
+NAME = lookup["NAME"]
+AUTHOR = lookup["AUTHOR"]
+AUTHOR_EMAIL = lookup["AUTHOR_EMAIL"]
+PACKAGE_URL = lookup["PACKAGE_URL"]
+KEYWORDS = lookup["KEYWORDS"]
+DESCRIPTION = lookup["DESCRIPTION"]
+LICENSE = lookup["LICENSE"]
+with open("README.md") as filey:
     LONG_DESCRIPTION = filey.read()
 
 
@@ -80,41 +83,41 @@ with open('README.md') as filey:
 
 if __name__ == "__main__":
 
-    INSTALL_REQUIRES = get_reqs(lookup, 'INSTALL_ALL')
-    INSTALL_USERVOICE = get_reqs(lookup, 'INSTALL_USERVOICE')
-    INSTALL_DISCOURSE = get_reqs(lookup, 'INSTALL_DISCOURSE') 
+    INSTALL_REQUIRES = get_reqs(lookup, "INSTALL_ALL")
+    INSTALL_USERVOICE = get_reqs(lookup, "INSTALL_USERVOICE")
+    INSTALL_DISCOURSE = get_reqs(lookup, "INSTALL_DISCOURSE")
 
-    setup(name=NAME,
-          version=VERSION,
-          author=AUTHOR,
-          author_email=AUTHOR_EMAIL,
-          maintainer=AUTHOR,
-          maintainer_email=AUTHOR_EMAIL,
-          packages=find_packages(), 
-          include_package_data=True,
-          zip_safe=False,
-          url=PACKAGE_URL,
-          license=LICENSE,
-          description=DESCRIPTION,
-          long_description=LONG_DESCRIPTION,
-          keywords=KEYWORDS,
-          install_requires = INSTALL_REQUIRES,
-          extras_require={
-
-              'all': [INSTALL_REQUIRES],
-              'uservoice': [INSTALL_USERVOICE],
-              'discourse': [INSTALL_DISCOURSE]
-
-          },
-          classifiers=[
-              'Intended Audience :: Science/Research',
-              'Intended Audience :: Developers',
-              'License :: OSI Approved :: MIT License',
-              'Programming Language :: C',
-              'Programming Language :: Python',
-              'Topic :: Software Development',
-              'Topic :: Scientific/Engineering',
-              'Operating System :: Unix',
-              'Programming Language :: Python :: 3',
-          ],
-          entry_points = {'console_scripts': [ 'helpme=helpme.client:main' ] })
+    setup(
+        name=NAME,
+        version=VERSION,
+        author=AUTHOR,
+        author_email=AUTHOR_EMAIL,
+        maintainer=AUTHOR,
+        maintainer_email=AUTHOR_EMAIL,
+        packages=find_packages(),
+        include_package_data=True,
+        zip_safe=False,
+        url=PACKAGE_URL,
+        license=LICENSE,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        keywords=KEYWORDS,
+        install_requires=INSTALL_REQUIRES,
+        extras_require={
+            "all": [INSTALL_REQUIRES],
+            "uservoice": [INSTALL_USERVOICE],
+            "discourse": [INSTALL_DISCOURSE],
+        },
+        classifiers=[
+            "Intended Audience :: Science/Research",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: C",
+            "Programming Language :: Python",
+            "Topic :: Software Development",
+            "Topic :: Scientific/Engineering",
+            "Operating System :: Unix",
+            "Programming Language :: Python :: 3",
+        ],
+        entry_points={"console_scripts": ["helpme=helpme.client:main"]},
+    )
