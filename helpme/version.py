9a5c4f50e4ace65de85ebc34647dabd59efca873
Vanessa Sochat <vsochat@stanford.edu>
2019-12-17 17:12:10 -0700

    bumping version
    Signed-off-by: Vanessa Sochat <vsochat@stanford.edu>

--- helpme/version.py
+++ helpme/version.py
@@ -17,7 +17,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 """
 
-__version__ = "0.0.39"
+__version__ = "0.0.40"
 AUTHOR = "Vanessa Sochat"
 AUTHOR_EMAIL = "vsochat@stanford.edu"
 NAME = "helpme"
